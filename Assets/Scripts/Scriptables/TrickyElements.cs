6a70baf6d12728d3a127fbcd26467412daaa56ec
Shoaib Wattoo <shoaib.wattoo@gmail.com>
2022-08-14 18:37:40 +0500

    Worked on UI Organizer Worked of UI Spawning System

--- Assets/Scripts/Scriptables/TrickyElements.cs
+++ Assets/Scripts/Scriptables/TrickyElements.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName = "TrickyElements", menuName = "ScriptableObjects/TrickyElements", order = 1)]
 public class TrickyElements : ScriptableObject
 {
+    [Header("Sprites")]
     public List<Sprite> tetrisSprites;
 
+    [Header("UI Screens")]
+    public SplashScreen splashScreen;
+    public HomeScreen homeScreen;
 }
---
+++ Assets/Scripts/UI/UiOrganizer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public interface IUiCanvas
+{
+    void ParentThisToCanvas(Transform toBeMadeChild);
+    Canvas GetCanvas();
+}
+
+public class UiOrganizer : MonoBehaviour, IUiCanvas {
+
+    [SerializeField]
+    private Canvas mainCanvas;
+
+    public Canvas GetCanvas()
+    {
+        return mainCanvas;
+    }
+
+    public Transform GetMainCanvasRoot()
+    {
+        return mainCanvas.transform;
+    }
+
+    public void ParentThisToCanvas(Transform toBeMadeChild)
+    {
+        toBeMadeChild.SetParent(mainCanvas.transform, false);
+    }
+}
