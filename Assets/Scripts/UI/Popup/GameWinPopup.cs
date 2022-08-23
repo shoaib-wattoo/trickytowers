40290ffa86d5d150eb47be22c9b259d3032f8b6e
Shoaib Wattoo <shoaib.wattoo@conovoinc.com>
2022-08-23 23:57:57 +0500

    Worked on Game lose and Game Win Popup

--- Assets/Scripts/UI/Popup/GameWinPopup.cs
+++ Assets/Scripts/UI/Popup/GameWinPopup.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
+using MiniClip.Challenge.States;
+using MiniClip.Challenge.ProjectServices;
 
 public class GameWinPopup : TrickyMonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Button restartButton, homeButton;
+    public TextMeshProUGUI coinText;
+
+    void Awake()
     {
-        
+        //restartButton.onClick.AddListener(OnClickRestartButton);
+        homeButton.onClick.AddListener(OnClickHomeButton);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        GiftCoin();
+    }
+
+    void OnClickRestartButton()
+    {
+        Services.GameService.gameStatus = GameStatus.TOSTART;
+        Services.GameService.DestryoGameplayManager();
+        Services.GameService.SetState(typeof(GamePlayState));
+    }
+
+    void OnClickHomeButton()
+    {
+        Services.GameService.SetState(typeof(MenuState));
+    }
+
+    void GiftCoin()
+    {
+        int coinsToDeduct = Random.Range(100, 500);
+        coinText.SetText(coinsToDeduct.ToString());
+        Services.PlayerService.SetCoins(coinsToDeduct);
     }
 }
