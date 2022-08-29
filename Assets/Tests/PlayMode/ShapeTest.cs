5ccf2b9fce3a18f0bdea4066dfa818fd9f9b01c6
Shoaib Wattoo <shoaib.wattoo@conovoinc.com>
2022-08-29 11:32:04 +0500

    Wrote play mode test

---
+++ Assets/Tests/PlayMode/ShapeTest.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class ShapeTest
+{
+    /*
+    [UnityTest]
+    public IEnumerator Shape_Right_Movement_Test()
+    {
+        var gameObject = new GameObject();
+        Vector3 startPos = gameObject.transform.position;
+        var shaep = gameObject.AddComponent<TrickyShape>();
+        shaep.MoveHorizontal(Vector2.right);
+        yield return new WaitForEndOfFrame();
+
+        Assert.AreEqual(new Vector3(startPos.x + 0.5f, startPos.y, startPos.z), gameObject.transform.position);
+    }
+
+    [UnityTest]
+    public IEnumerator Shape_Left_Movement_Test()
+    {
+        var gameObject = new GameObject();
+        Vector3 startPos = gameObject.transform.position;
+        var shaep = gameObject.AddComponent<TrickyShape>();
+        shaep.MoveHorizontal(Vector2.left);
+        yield return new WaitForEndOfFrame();
+
+        Assert.AreEqual(new Vector3(startPos.x + 0.5f, startPos.y, startPos.z), gameObject.transform.position);
+    }
+
+    [UnityTest]
+    public IEnumerator Shape_Rotation_Test()
+    {
+        var gameObject = new GameObject();
+        Quaternion startRotation = gameObject.transform.rotation;
+        var shaep = gameObject.AddComponent<TrickyShape>();
+        shaep.RotateClockWise(true);
+        yield return new WaitForEndOfFrame();
+        float angle = Quaternion.Angle(startRotation, gameObject.transform.rotation);
+
+        Assert.AreEqual(angle,90f);
+    }
+    */
+}
